2d5fb113f09c77ffc56bb11cb3003a87ab6f3080
YS Pang <jasonp.space@gmail.com>
2024-09-06 15:35:13 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Pang Yan Shen",
+  title: "Data Enthusiast",
+  email: "pang.yanshen@gmail.com",
+  gitHub: "yanshenp",
+  instagram: "",
+  linkedIn: "yanshenpang",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
