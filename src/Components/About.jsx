41a66a37e3abe81b2aab22a0fb94be2286910482
YS Pang <jasonp.space@gmail.com>
2024-09-06 07:48:32 +0000

    Update details

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -23,17 +23,16 @@ const imageAltText = "blue sea background";
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  `I graduated with a major in Biotechnology, where I developed a strong foundation in genetics and chemistry. \n\
-  During my undergraduate studies, I had the opportunity to gain hands-on experience in the agricultural field, \n\
-  which broadened my understanding of practical applications in science. After completing my degree, \n\
-  I transitioned into a role as a Technical Sales Executive at an oleo chemical company. In this position, \n\ 
-  I was responsible for bridging the gap between technical product specifications and customer needs. \n\
-  Realizing the growing importance of data in driving business decisions, I then decided to further my education by \n\
-  pursuing a Master of Science in Data Science and Business Analytics. This program equipped me with advanced skills in data analysis, machine learning, and business strategy. \n\
-  I have learned to leverage data to provide actionable insights and solve complex problems, which I believe is crucial in today’s data-driven world. \n\
-  Combining my background in science, technical sales, and data science, I bring a unique perspective and a versatile skill set to the table. \n\ 
-  I am particularly passionate about using data to make informed decisions that can drive business growth and innovation. \n\
+const description = `I graduated with a major in Biotechnology, where I developed a strong foundation in genetics and chemistry. 
+  During my undergraduate studies, I had the opportunity to gain hands-on experience in the agricultural field,
+  which broadened my understanding of practical applications in science. After completing my degree, 
+  I transitioned into a role as a Technical Sales Executive at an oleo chemical company. In this position, 
+  I was responsible for bridging the gap between technical product specifications and customer needs. 
+  Realizing the growing importance of data in driving business decisions, I then decided to further my education by 
+  pursuing a Master of Science in Data Science and Business Analytics. This program equipped me with advanced skills in data analysis, machine learning, and business strategy. 
+  I have learned to leverage data to provide actionable insights and solve complex problems, which I believe is crucial in todays data-driven world.
+  Combining my background in science, technical sales, and data science, I bring a unique perspective and a versatile skill set to the table.
+  I am particularly passionate about using data to make informed decisions that can drive business growth and innovation. 
   I am excited about the opportunity to apply my skills and experiences to contribute to your team.`;
 
 /**
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,7 +31,8 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "Predicting Loan Default in Peer-to-Peer Lending: LightGBM with Boruta Feature Selection Approach",
+    title:
+      "Predicting Loan Default in Peer-to-Peer Lending: LightGBM with Boruta Feature Selection Approach",
     description:
       "This project utilized the LightGBM algorithm alongside the Boruta feature selection technique to identify the most critical features and develop a predictive model for forecasting loan defaults in the Peer-to-Peer (P2P) lending sector. The goal was to create a model that accurately predicts loan defaults, benefiting lenders and P2P lending platforms.",
     url: "https://github.com/yanshenp/p2p-lending-loan-default-predictioni",
